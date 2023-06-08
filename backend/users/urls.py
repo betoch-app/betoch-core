10b67b2cd8b582f6928fe1f7cb574ee166647f93
DESKTOP-KMC1ECQ\meserert <kassaye.meseret21@gmail.com>
2023-06-08 18:24:37 +0300

    minor changes

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -3,7 +3,6 @@ from .views import (AuthUserRegistrationView, AuthUserLoginView)
 from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path('token/obtain', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
     path('token/refresh', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('register', AuthUserRegistrationView.as_view(), name='register'),
     path('login', AuthUserLoginView.as_view(), name='login'),
