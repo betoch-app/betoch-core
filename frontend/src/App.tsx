898d7b972dc295d022c97b06f79c9bb7f2999747
DESKTOP-KMC1ECQ\meserert <kassaye.meseret21@gmail.com>
2023-06-09 13:46:43 +0300

    BC-105 | antd theme is created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,26 @@
+import { ConfigProvider, Button } from 'antd';
 import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
 import GenericSpinner from './modules/core/views/components/GenericSwipper/GenericSpinner';
 import './styles/scss/main.scss';
-
+import _tokens from './styles/_theme_tokens.json';
 const loader = <GenericSpinner />;
-const router = <div>Router</div>;
+const router = (
+  <div>
+    <Button type="default">Button</Button>
+  </div>
+);
 function App() {
   return (
     <BrowserRouter key={''}>
-      <Suspense fallback={loader}>{router}</Suspense>
+      <ConfigProvider
+        theme={{
+          token: _tokens,
+        }}
+      >
+        <Suspense fallback={loader}>{router}</Suspense>
+      </ConfigProvider>
     </BrowserRouter>
   );
 }
